f1cb06804da4a8f180f5cea3f9c92370103d4027
sdp <sven@svendepreter.be>
2016-12-15 15:29:20 +0100

    Initial Commit

---
+++ xls2csv.ps1
@@ -0,0 +1,37 @@
+﻿
+# Written by Sven De Preter 
+# Convert XLS to CSV - PowerShell Version
+
+Function ExcelToCSV($file){
+
+    # Input : $file, a full path filename
+    # Output : files with the same path and name as $file, with the worksheet name and .csv appended
+
+
+    # Create new Excel Application object
+    $ExcelObject = New-Object -ComObject Excel.Application
+    $ExcelObject.Visible = $false
+    $ExcelObject.DisplayAlerts=$false
+
+    # Open the Excelfile 
+    $workbook = $ExcelObject.Workbooks.Open($file.Fullname)
+        
+    # for each worksheet, save the file as a csv
+    foreach ($worksheet in $workbook.Worksheets)
+    {
+        $worksheet.SaveAs(($file.Fullname -replace '.xlsx$', '') + "-" + ($worksheet.name) + ".csv",6)
+    }
+
+    # close and quit excel
+    $workbook=$excel.workbooks.close()
+    $ExcelObject.quit();
+}
+
+
+# Files in the $Directory path will be processed
+$Directory = "C:\\TEMP\\"
+
+Foreach ($files in (Get-ChildItem -path $Directory -Filter "*.xlsx"))
+{
+    ExcelToCSV($files)
+}
